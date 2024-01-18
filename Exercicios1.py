251d3dc3e0d594b5e0585a6025bf086ad3a0a1cf
Jessica Souza <jessicabs.adm@gmail.com>
2024-01-17 20:05:02 -0700

    teste

--- Exercicios1.py
+++ Exercicios1.py
@@ -1,1 +1,28 @@
-##coments
+#Exercicio 1
+# Solicitar dois numeros ao usuario
+numero1 = float(input("Digite o primeiro numero: "))
+numero2 = float(input("Digite o segundo numero: "))
+
+# Principais operacoes
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+# Exibir os resultados
+print(f"Soma: {soma}")
+print(f"Subtracao: {subtracao}")
+print(f"multiplicacao: {multiplicacao}")
+print(f"divisao: {divisao}")
+
+#Exercicio 2
+#Solicitar a data de nascimento
+data_nascimento = input("Digite sua data de nascimento")
+
+#Exercicio 3
+#Solicitar a quantidade de quilometros
+quilometros = float(input("Digite a quantidade de quilometros"))
+
+
+
+
--- Exercicios2.py
+++ Exercicios2.py
@@ -1,11 +1,1 @@
-# Mensagens iniciais para o usuario
-'''Mensagens iniciais para o usuario'''
-
-print ('Ola mundo!')
-print ('Esse e o meu primeiro codigo em Python!')
-numero = int(input('Digite seu numero: '))
-print(numero)
-
-#Tipo com virgula - float
-numero = float(input('Digite um numero float: '))
-print(numero)
+##coments
