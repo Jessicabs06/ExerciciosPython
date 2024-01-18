30357c8c585e4b53567d9d621338b68ef92bf8db
Jessica Souza <jessicabs.adm@gmail.com>
2024-01-17 21:14:42 -0700

    Alteracoes1

--- Exercicios1.py
+++ Exercicios1.py
@@ -17,12 +17,63 @@ print(f"divisao: {divisao}")
 
 #Exercicio 2
 #Solicitar a data de nascimento
-data_nascimento = input("Digite sua data de nascimento")
+
 
 #Exercicio 3
 #Solicitar a quantidade de quilometros
-quilometros = float(input("Digite a quantidade de quilometros"))
+quilometros = float(input("Digite a quantidade de quilometros "))
+
+#Conversoes
+metros = quilometros * 1000
+centimetros = quilometros * 100000
+milimetros = quilometros * 1000000
+
+#Resultados
+print(f"{quilometros} quilometros equivalem a:{metros} metros ")
+print(f"{quilometros} quilometros equivalem a:{centimetros} centimetros ")
+print(f"{quilometros} quilometros equivalem a:{milimetros} milimetros ")
+
+#Exercicio 4
+#Quantidade de litros de combustivel consumidos
+litros_combustivel = float(input("Digite a quantidade de litros consumidos: "))
+distancia = float(input("Qual a distancia percorrida? "))
+
+#Conversao 
+consumo_medio = distancia / litros_combustivel
+
+#resultados
+print(f"O consumo medio e de {consumo_medio} : km/l")
+
+#Exercicio 5
 
 
+#Exercicio 6
 
+
+#Exercicio 7
+#Solicitar o peso e altura do ususario
+peso = float(input("Digite seu peso em kg: "))
+altura =  float(input("Digite sua altura: "))
+
+#conversao
+IMC = peso / altura*2
+
+#resultados
+print(f"O seu IMC e : {IMC}.")
+
+#Exercicio 8 
+#Solicitar salario por hora
+salario = float(input("Digite seu salario por hora: "))
+horas_trabalhadas =  float(input("Digite as horas trabalhadas no mes: "))
+
+#Conversao
+Salario_mes = salario * horas_trabalhadas
+
+#Resultado
+print(f"O seu salario mensal e {Salario_mes}.")
+
+#Exercicio 9
+
+
+#Exercicio 10
 
