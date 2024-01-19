522c10542236247ca011a33b7146c09795027f1b
Jessica Souza <jessicabs.adm@gmail.com>
2024-01-18 18:22:16 -0700

    Alteracoes 3

--- Exercicios2.py
+++ Exercicios2.py
@@ -37,3 +37,36 @@ if nota < 7:
     print("Reprovados. ")
 
 #Exercicio 5
+#Solicita os lados do triangulo
+lado1 = float(input("Digite o primeiro lado do triangulo. "))
+lado2 = float(input("Digite o segundo lado do triangulo. "))
+lado3 = float(input("Digite o terceiro lado do triangulo. "))
+
+#Verifica os lados do triangulo
+if lado1 == lado2 == lado3:
+    print("O triangulo e equilatero. ")
+if lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+    print("O triangulo e isosceles. ")
+else :
+    print("O triangulo e escaleno. ")
+
+#Exercicio 6
+
+
+#Exercicio 7
+#Solicita a idade do usuario
+idade = float(input("Digite sua idade. "))
+
+#Verificacao
+if idade <= 12 :
+    print("Voce e uma crianca. ")
+if idade == 13 < 18:
+    print("Voce e um adolescente. ")
+if idade == 18 < 65: 
+    print("Voce e um adulto. ")
+if idade >= 66 :
+    print("Voce e um idoso. ")
+
+#Exercicio 8
+#Solicitar 3 numeros
+
